3fd330e4007cdc4cc9adc162371c4cfc6b878bc3
yvngchery <alex.chery@kippstudent.org>
2023-01-18 16:42:15 +0000

    updating platform neo

--- projects/platformer-neo/platformer.js
+++ projects/platformer-neo/platformer.js
@@ -30,7 +30,7 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 
-   
+   createPlatform(385, 499,0.5);
 
     
     
