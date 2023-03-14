6c3af22af6635e44a958b980ed6d3136b9aac957
yvngchery <alex.chery@kippstudent.org>
2023-03-14 16:32:07 +0000

    runtime

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -27,7 +27,6 @@ var background = function (window) {
 
         // ANIMATION VARIABLES HERE:
 
-
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -36,9 +35,9 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth, canvasHeight, 'yellow');
+            var backgroundFill = draw.rect(canvasWidth, canvasHeight, 'black');
             background.addChild(backgroundFill);
-            // draw.rect(width, height, fillColor);
+            // draw.rect(width, 100, fillColor);
             // TODO: 3 - Add a moon and starfield
             var moon = draw.bitmap("img/moon.png");
             moon.x = 300;
@@ -47,15 +46,24 @@ var background = function (window) {
             moon.scaleY = 0.5;
             background.addChild(moon);
 
+            for (var i = 0; i < 100; i++) {
+                var circle = draw.circle(5, "white", "LightGray", 2);
+                circle.x = canvasWidth * Math.random();
+                circle.y = groundY * Math.random();
+                background.addChild(circle);
+            }
             // TODO 5: Part 1 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
 
 
             // TODO 4: Part 1 - Add a tree
-
+            tree = draw.bitmap("img/tree.png");
+            tree.x = 0;
+            tree.y = groundY - 30;
+            background.addChild(tree);
 
         } // end of render function - DO NOT DELETE
 
-
+        var tree;
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
