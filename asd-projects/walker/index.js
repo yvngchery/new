a5d3d61f276aea212b34aac2a4e225bec0ee2f1d
yvngchery <alex.chery@kippstudent.org>
2023-06-14 20:49:41 +0000

    walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -11,12 +11,16 @@ function runProgram(){
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   
+  var KEY ={
+    "ENTER": 13,
+  }
+  // CONTINUE RIGHT HERE ^  
   // Game Item Objects
 
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  $(document).on('keydown', handleKeyDown);                           // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -34,8 +38,8 @@ function runProgram(){
   /* 
   Called in response to events.
   */
-  function handleEvent(event) {
-
+  function handleKeyDown(event) {
+console.log()
   }
 
   ////////////////////////////////////////////////////////////////////////////////
